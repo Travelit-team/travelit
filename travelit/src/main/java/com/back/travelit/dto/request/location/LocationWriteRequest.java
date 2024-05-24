213f59b7698e02525b4a1a1c247996363db0cd96
limseonghyun <sksrpf1126@naver.com>
2024-05-24 16:06:23 +0900

    refactor: 부제목 최대 길이 15 -> 30 변경

--- travelit/src/main/java/com/back/travelit/dto/request/location/LocationWriteRequest.java
+++ travelit/src/main/java/com/back/travelit/dto/request/location/LocationWriteRequest.java
@@ -27,7 +27,7 @@ public class LocationWriteRequest {
     @Size(min = 2, max = 15, message = "제목의 길이는 2~15 글자 사이어야 합니다.")
     private String title;
 
-    @Size(min = 2, max = 15, message = "부제목의 길이는 2~15 글자 사이어야 합니다.")
+    @Size(min = 2, max = 30, message = "부제목의 길이는 2~30 글자 사이어야 합니다.")
     private String subTitle;
 
     @Size(min = 1, max =10000, message = "상세 내용은 1 ~ 10000 글자 사이어야 합니다.")
