942a9089bbee5339b9ecf76f68080283a2f86798
jinny <pjiny92@gmail.com>
2024-05-25 17:19:52 +0900

    refactor: planner first-step calender 주석 삭제

--- travelit/src/main/resources/static/js/planner/plan-first-calender.js
+++ travelit/src/main/resources/static/js/planner/plan-first-calender.js
@@ -1,44 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-    var startDate; // 선택한 시작일을 저장하는 변수
-    var endDate; // 선택한 종료일을 저장하는 변수
-
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'multiMonthYear',
-        selectable: true,
-        multiMonthMaxColumns: 1, // force a single column
-        dateClick: function(info) {
-             alert('clicked ' + info.dateStr);
-        },
-        select: function(info) {
-            startDate = info.startStr;
-            endDate = info.end.toISOString().split('T')[0];
-            $("#startDate").val(startDate);
-            $("#endDate").val(endDate);
-            console.log(startDate);
-            console.log(endDate);
-
-            // 기존 선택 영역 제거
-            var selectedElements = document.querySelectorAll('.selected-range');
-            selectedElements.forEach(function(element) {
-                element.classList.remove('selected-range');
-            });
-
-            // 선택한 영역 강조
-            var currentDate = new Date(startDate);
-
-            while (currentDate <= endDate) {
-                var formattedDate = currentDate.toISOString().split('T')[0];
-                var dayEl = calendarEl.querySelector(`[data-date="${formattedDate}"]`);
-                if (dayEl) {
-                    dayEl.classList.add('selected-range');
-                }
-                currentDate.setDate(currentDate.getDate());
-            }
-        }
-    });
-    calendar.render();
-});*/
 
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
@@ -46,9 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var startDate, endDate;
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'multiMonthYear',
+        initialView: 'dayGridMonth',
         selectable: true,
-        multiMonthMaxColumns: 1, // 한 열로 강제 설정
         dateClick: function(info) {
             if (isSelectingStartDate) {
                 startDate = info.dateStr;
