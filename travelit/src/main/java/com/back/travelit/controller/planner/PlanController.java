bc17b6f48fb1dfd54c40683919eda12c9600152d
jinny <pjiny92@gmail.com>
2024-05-27 15:49:18 +0900

    feat: planner second-step 플래너 제목,일정,지역 back-end 구현

--- travelit/src/main/java/com/back/travelit/controller/planner/PlanController.java
+++ travelit/src/main/java/com/back/travelit/controller/planner/PlanController.java
@@ -1,15 +1,20 @@
 package com.back.travelit.controller.planner;
 
-import com.back.travelit.dto.request.planner.PlanCreateReqDTO;
+import com.back.travelit.dto.request.planner.PlanCreateReq;
+import com.back.travelit.dto.response.planner.PlanLocCodeRes;
 import com.back.travelit.service.planner.PlanService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -19,6 +24,8 @@ public class PlanController {
 
     @Autowired
     private PlanService planService;
+    private Model model;
+    private ModelAndView modelAndView;
 
     //플래너 만들기 페이지
     @GetMapping("/plan-first")
@@ -28,14 +35,18 @@ public class PlanController {
 
     //플래너 만들기
     @PostMapping("/plan-make")
-    public String makePlan(@ModelAttribute("createReqDTO)") PlanCreateReqDTO createReqDTO){
-        planService.setMakePlan(createReqDTO);
+    public String makePlan(@ModelAttribute("createReqDTO)") PlanCreateReq createReqDTO, Model model){
+        int planId = planService.setMakePlan(createReqDTO);
+        log.info("planId");
+        model.addAttribute("createReqDTO", createReqDTO);
+        List<PlanLocCodeRes> states = planService.selectAllLocCode(createReqDTO.getStateCode());
+        model.addAttribute("states", states);
         return "/planner/plan-second";
     }
 
-    //스케줄 생성
+   /* //스케줄 생성
     @PostMapping("/plan-shed")
     public String makeShed(){
         return "/planner/plan-detail";
-    }
+    }*/
 }
