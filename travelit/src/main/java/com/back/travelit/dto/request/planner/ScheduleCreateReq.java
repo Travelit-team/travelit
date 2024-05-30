844c9966c8b29533864c8931bd2d78aa7cb1beab
jinny <pjiny92@gmail.com>
2024-05-30 22:55:43 +0900

    refactor: 변수명 변경

--- travelit/src/main/java/com/back/travelit/dto/request/planner/ScheduleCreateReq.java
+++ travelit/src/main/java/com/back/travelit/dto/request/planner/ScheduleCreateReq.java
@@ -1,13 +1,25 @@
 package com.back.travelit.dto.request.planner;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class ScheduleCreateReq {
 
+    @Setter
+    private int schedId;
+
+
+    //플래너 아이디
     private int planId;
-
+    //플래너 일차
     private int planDay;
-
+    //장소 아이디
     private int locInfoId;
+
+    public ScheduleCreateReq(int planId, int planDay, int locInfoId) {
+        this.planId = planId;
+        this.planDay = planDay;
+        this.locInfoId = locInfoId;
+    }
 }
