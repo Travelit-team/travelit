3c51944b3edbcaaa5bc12f2db58656c3e48ad67a
limseonghyun <sksrpf1126@naver.com>
2024-06-03 11:31:22 +0900

    refactor: 플래너 first 페이지 디자인 수정 및 기능 추가

--- travelit/src/main/resources/static/js/planner/plan-first-calender.js
+++ travelit/src/main/resources/static/js/planner/plan-first-calender.js
@@ -7,15 +7,37 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
         selectable: true,
+        titleFormat: function (date) {
+            year = date.date.year;
+            month = date.date.month + 1;
+
+            return year + "년 " + month + "월";
+        },
         dateClick: function(info) {
             if (isSelectingStartDate) {
                 startDate = info.dateStr;
                 $("#startDate").val(startDate);
+                $("#endDate").val(startDate);
                 isSelectingStartDate = false;
                 alert('시작일 선택: ' + startDate);
                 console.log(startDate);
             } else {
                 endDate = info.dateStr;
+
+                // 시작일보다 종료일이 빠른 경우 알림
+                if (new Date(endDate) < new Date(startDate)) {
+                    alert('종료일은 시작일보다 빠를 수 없습니다.');
+                    return;
+                }
+
+                // 종료일이 시작일로부터 2주를 초과하는 경우 알림
+                var twoWeeksFromStartDate = new Date(startDate);
+                twoWeeksFromStartDate.setDate(twoWeeksFromStartDate.getDate() + 14);
+                if (new Date(endDate) > twoWeeksFromStartDate) {
+                    alert('종료일은 시작일로부터 최대 2주까지만 선택할 수 있습니다.');
+                    return;
+                }
+
                 $("#endDate").val(endDate);
                 isSelectingStartDate = true;
                 alert('종료일 선택: ' + endDate);
@@ -47,6 +69,15 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     calendar.render();
+
+    $(".fc .fc-daygrid-day.fc-day-today").removeClass("fc-day-today");
+
+    $(".fc-next-button").click(function() {
+        highlightRange(startDate, endDate);
+    });
+    $(".fc-prev-button").click(function() {
+        highlightRange(startDate, endDate);
+    });
 
     // 날짜 클릭 시 다시 선택 가능하게 설정
     calendarEl.addEventListener('click', function(event) {
