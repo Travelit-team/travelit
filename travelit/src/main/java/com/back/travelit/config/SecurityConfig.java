054577f1bd2d7c1f869536fcc40ffeaf6d67e235
HeegeneChae <heegenechae@gmail.com>
2024-06-04 02:44:10 +0900

    2024-06-04 소스 버그 수정

--- travelit/src/main/java/com/back/travelit/config/SecurityConfig.java
+++ travelit/src/main/java/com/back/travelit/config/SecurityConfig.java
@@ -11,11 +11,13 @@ import com.back.travelit.security.handler.CustomAuthenticationEntryPoint;
 import jakarta.servlet.http.HttpServletRequest;
 
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -121,5 +123,11 @@ public class SecurityConfig {
 */
 
         return http.build();
+    }
+    
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
