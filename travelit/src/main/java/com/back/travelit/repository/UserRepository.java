9b8e774d0e8077015a6826e9130f14a7a96912d6
limseonghyun <sksrpf1126@naver.com>
2024-06-03 10:35:38 +0900

    Revert "누락소스 및 오류 정리"
    This reverts commit 06e44047440e4b37e84d31c466746150eb4e0085.

--- travelit/src/main/java/com/back/travelit/repository/UserRepository.java
+++ travelit/src/main/java/com/back/travelit/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package com.back.travelit.repository;
 
-import com.back.travelit.domain.user.UserEntity;
+import com.back.travelit.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    Optional<UserEntity> findByLoginID(String LOGIN_ID);
+    UserEntity findByLoginID(String LOGIN_ID);
 
 }
